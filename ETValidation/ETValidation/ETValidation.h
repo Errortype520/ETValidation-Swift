f450b4b4bf699fb2886bbf5b37facee98238f1a6
Joseph Burgess <joe@delaneyrim.com>
2015-10-14 11:52:39 -0400

    Framework support

---
+++ ETValidation/ETValidation/ETValidation.h
@@ -0,0 +1,19 @@
+//
+//  ETValidation.h
+//  ETValidation
+//
+//  Created by Joseph Burgess on 10/14/15.
+//  Copyright © 2015 Joseph Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ETValidation.
+FOUNDATION_EXPORT double ETValidationVersionNumber;
+
+//! Project version string for ETValidation.
+FOUNDATION_EXPORT const unsigned char ETValidationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ETValidation/PublicHeader.h>
+
+
